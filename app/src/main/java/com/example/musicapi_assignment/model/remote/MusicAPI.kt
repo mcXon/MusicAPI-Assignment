116b9c0d90b5205f77e72b7152326ea4dc890937
mcXon <kinder.trunco@gmail.com>
2022-11-28 11:09:11 -0600

    Small changes on fragments

--- app/src/main/java/com/example/musicapi_assignment/model/remote/MusicAPI.kt
+++ app/src/main/java/com/example/musicapi_assignment/model/remote/MusicAPI.kt
@@ -1,7 +1,6 @@
 package com.example.musicapi_assignment.common
 
 import com.example.musicapi_assignment.model.*
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
